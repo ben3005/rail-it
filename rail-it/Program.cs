44b3a96d7ed5938fc0d5c9b6178dd7a345257c96
Ben Marshall <ben3005@hotmail.com>
2017-01-18 21:55:57 +0000

    inital commit

---
+++ rail-it/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rail_it
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TaskRunner.runTask(tmpTask, 20, 4);
+			Console.ReadKey();
+		}
+
+		static void tmpTask()
+		{
+			Console.WriteLine("task running on thread {0} ", Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+}
---
+++ rail-it/TaskRunner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rail_it
+{
+	public class TaskRunner
+	{
+		public static void runTask(Action theMethod, int totalRuns = 1, int threads = 1)
+		{
+			if (totalRuns <= 0)
+			{
+				throw new ArgumentException("Task must run at least once");
+			}
+			if (threads <= 0)
+			{
+				throw new ArgumentException("Task must run on at least one thread");
+			}
+
+			var currentScheduler = new LimitedConcurrencyLevelTaskScheduler(threads);
+			var taskFactory = new TaskFactory(currentScheduler);
+			CancellationTokenSource cts = new CancellationTokenSource();
+
+			object lockObj = new object();
+			List<Task> tasks = new List<Task>();
+			for (int j = 0; j < totalRuns; j++)
+			{
+				lock (lockObj)
+				{
+					tasks.Add(taskFactory.StartNew(() => theMethod(), cts.Token));
+				}
+			}
+			Task.WaitAll(tasks.ToArray());
+		}
+	}
+}
